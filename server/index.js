0f2aa4d676bef42193a454f2f167bb1c8d7c1cf6
Jasur <jasafayz01@gmail.com>
2023-04-10 18:09:13 +0500

    fix: bug fix

--- server/index.js
+++ server/index.js
@@ -4,20 +4,19 @@ const config = require('config')
 const http = require('http');
 const authRouter = require('./routes/auth.routes')
 const corsModdleware = require('./middleware/cors.middleware')
-const filePathMiddleware = require('./middleware/filePath.middleware')
 
+const { createServer } = require("http");
 
 const app = express()
 const PORT = process.env.PORT || config.get('serverPort')
 
-const path = require('path')
 
 const socketIo = require('socket.io')
 const server = http.createServer(app)
 // const io = socketIo(server, {
-//     cors: {
-//         origin: [`http://localhost:3000`]
-//     }
+//     // cors: {
+//     //     origin: [`http://localhost:3000`]
+//     // }
 // })
 // const io = require('socket.io')(`http://localhost:${PORT}`)
 
@@ -29,13 +28,13 @@ app.use('/api/auth', authRouter)
 // io.on("connection", (socket) => {
 //     console.log(`User Connected: ${socket.id}`);
   
-//     socket.on("join_room", (data) => {
-//       socket.join(data);
-//     });
+//     // socket.on("join_room", (data) => {
+//     //   socket.join(data);
+//     // });
   
 //     socket.on("send_message", (data) => {
 //         console.log(data);
-//         socket.to(data.room).emit("receive_message", data);
+//         // socket.to(data.room).emit("receive_message", data);
 //     //   socket.to(data.room).emit("receive_message", data);
 //     });
 //   });
